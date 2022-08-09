04629a89ddd2f3d418cd9cddf565b0f461ce8cdc
alien2804 <109579232+alien2804@users.noreply.github.com>
2022-08-09 13:37:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,16 +54,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94774579505'] //ur owner number
-global.ownername = "🎸 KAVEESHA " //ur owner name
-global.ytname = "YT: Nero" //ur yt chanel name
-global.socialm = "GitHub: Nero" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['+2348100236360'] //+2348100236360
+global.ownername = "🎸 BASHIR " //Bashir
+global.ytname = "YT: Nero" //Alien Graphics
+global.socialm = "GitHub: Nero" //ag_toons
+global.location = "India, Mizoram, Aizawl" //Nigeria, Lagos, Agege
 
 //bot bomdy 
-global.owner = ['774579505']
-global.ownertag = '774579505' //ur tag number
-global.botname = '✫ ✫ ✫𝙳𝙰𝚁𝙺 𝙽𝙴𝚁𝙾✫ ✫ ✫' //ur bot name
+global.owner = ['8129708110']
+global.ownertag = '774579505' //8129708110
+global.botname = '✫ ✫ ✫𝙳𝙰𝚁𝙺 𝙽𝙴𝚁𝙾✫ ✫ ✫' //Alien BOT
 global.linkz = "https://github.com/Kaveeshasithum/DARK-NERO-BOT.git" //your theme url which will be displayed on whatsapp
 global.websitex = "https://github.com/Kaveeshasithum/DARK-NERO-BOT.git" //ur website to be displayed
 global.botscript = 'https://github.com/Kaveeshasithum/DARK-NERO-BOT.git' //script link
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['774579505'] //ur premium numbers
+global.premium = ['+2348100236360'] //+2348100236360
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
